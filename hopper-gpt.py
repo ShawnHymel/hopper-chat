2b49de03a8634eeede444fa2193a2bd0b8926df3
ShawnHymel <5232145+ShawnHymel@users.noreply.github.com>
2024-04-20 18:54:11 -0600

    First draft of smart speaker done

--- hopper-gpt.py
+++ hopper-gpt.py
@@ -11,11 +11,16 @@ import time
 import sys
 import json
 import os
+from collections import deque
 
 from dotenv import load_dotenv
+import numpy as np
+import resampy
 import sounddevice as sd
+import soundfile as sf
 from vosk import Model, KaldiRecognizer
 import openai
+from TTS.api import TTS
 
 #---------------------------------------------------------------------------------------------------
 # Settings
@@ -27,16 +32,60 @@ DEBUG = True
 AUDIO_INPUT_INDEX = 2
 AUDIO_OUTPUT_INDEX = 1
 
-# Set wake word or phrase
-WAKE_PHRASE = "hey hopper"
+# Volume (1.0 = normal, 2.0 = double volume)
+AUDIO_OUTPUT_VOLUME = 1.0
+
+# Sample rate (determined by speaker hardware)
+AUDIO_OUTPUT_SAMPLE_RATE = 48000
+
+# Set notification sound (when wake phrase is heard). Leave blank for no notification sound.
+NOTIFICATION_PATH = "./sounds/cowbell.wav"
+
+# Set wake words or phrases
+WAKE_PHRASES = [
+    "hey hopper",
+    "a hopper",
+]
 
 # Set action phrases
-ACTION_CLEAR_HISTORY = ["clear history", "clear chat history"]
-ACTION_STOP = ["stop", "nevermind", "never mind"]
+ACTION_CLEAR_HISTORY = [    # Clear chat history
+    "clear history",
+    "clear chat history",
+]
+ACTION_STOP = [             # Return to waiting for wake phrase
+    "stop",
+    "stop listening",
+    "nevermind", 
+    "never mind",
+]
 
 # ChatGPT settings
-GPT_API_KEY = "" # Leave blank to load from OPENAI_API_KEY environment variable
-GPT_MODEL = "gpt-4"
+GPT_API_KEY = ""            # Leave blank to load from OPENAI_API_KEY environment variable
+GPT_MODEL = "gpt-3.5-turbo"
+GPT_MAX_HISTORY = 20        # Number of prompts and replies to remember
+GPT_MAX_REPLY_SENTENCES = 2 # Max number of sentences to respond with (0 is infinite)
+GPT_GET_FULL_REPLY = False  # Get full reply after getting the summary
+
+# TTS settings
+TTS_ENABLE = True
+TTS_MODEL = "tts_models/en/ljspeech/speedy-speech"  # Run `tts --list_models` to see options
+TTS_MODEL_SAMPLE_RATE = 22050   # Determined by model
+
+#---------------------------------------------------------------------------------------------------
+# Classes
+
+class FixedSizeQueue:
+    """
+    Fixed size array with FIFO
+    """
+    def __init__(self, max_size):
+        self.queue = deque(maxlen=max_size)
+
+    def push(self, item):
+        self.queue.append(item)
+
+    def get(self):
+        return list(self.queue)
 
 #---------------------------------------------------------------------------------------------------
 # Functions
@@ -45,9 +94,69 @@ def callback_record(in_data, frames, time, debug):
     """
     Fill global input queue with audio data
     """
+    global in_q
+
     if debug:
         print(status, file=sys.stderr)
     in_q.put(bytes(in_data))
+
+def wait_for_stt(sd, recognizer):
+    """
+    Wait for STT to hear something and return the text
+    """
+
+    global in_q
+
+    # Listen for wake word/phrase
+    with sd.RawInputStream(
+        dtype="int16",
+        channels=1,
+        callback=callback_record
+    ):
+        if DEBUG:
+            print("Listening...")
+    
+        # Perform keyword spotting
+        while True:
+            data = in_q.get()
+            if recognizer.AcceptWaveform(data):
+
+                # Perform speech-to-text (STT)
+                result = recognizer.Result()
+                result_dict = json.loads(result)
+                result_text = result_dict.get("text", "")
+
+                return result_text
+
+def query_chat(msg):
+    """
+    Send message to chat backend (ChatGPT) and return response text
+    """
+
+    global msg_history
+
+    # Add prompt to message history
+    msg_history.push({
+        "role": "user",
+        "content": msg,
+    })
+
+    print(msg_history.get())
+
+    # Query ChatGPT
+    completion = gpt_client.chat.completions.create(
+        model=GPT_MODEL,
+        messages=msg_history.get()
+    )
+    
+    # Extract text reply and append to message history
+    reply = completion.choices[0].message.content
+    msg_history.push({
+        "role": "assistant",
+        "content": reply,
+    })
+
+    return reply
 
 #---------------------------------------------------------------------------------------------------
 # Main
@@ -67,6 +176,17 @@ sample_rate = int(device_info["default_samplerate"])
 # Display input device info
 if DEBUG:
     print(f"Input device info: {json.dumps(device_info, indent=2)}")
+
+# Load notification sound into memory
+if NOTIFICATION_PATH:
+    notification_wav, notification_sample_rate = sf.read(NOTIFICATION_PATH)
+    notification_wav = np.array(notification_wav) * AUDIO_OUTPUT_VOLUME
+    notification_wav = resampy.resample(
+        notification_wav,
+        notification_sample_rate,
+        AUDIO_OUTPUT_SAMPLE_RATE
+    )
+
 
 # Set up queue and callback
 in_q = queue.Queue()
@@ -93,81 +213,79 @@ if DEBUG:
 # Initialize ChatGPT client
 gpt_client = openai.OpenAI(api_key=gpt_api_key)
 
+# Initialize TTS
+if TTS_ENABLE:
+    tts = TTS(model_name=TTS_MODEL, progress_bar=False)
+
 # Superloop
-msg_history = []
+msg_history = FixedSizeQueue(GPT_MAX_HISTORY)
 while True:
     
-    # Listen for wake word/phrase
-    with sd.RawInputStream(
-        dtype="int16",
-        channels=1,
-        callback=callback_record
-    ):
-        listening_for_ww = True
+    # Listen for wake word or phrase
+    text = wait_for_stt(sd, recognizer)
+    if DEBUG:
+        print(f"Heard: {text}")
+    if text in WAKE_PHRASES:
         if DEBUG:
-            print("Listening for keyword...")
-    
-        # Perform keyword spotting
-        while listening_for_ww:
-            data = in_q.get()
-            if recognizer.AcceptWaveform(data):
+            print(f"Wake phrase detected.")
+    else:
+        continue
 
-                # Perform speech-to-text (STT)
-                result = recognizer.Result()
-                result_dict = json.loads(result)
-                result_text = result_dict.get("text", "")
-                if result_text != "":
-                    if DEBUG:
-                        print(f"Heard: {result_text}")
-                    if result_text == WAKE_PHRASE:
-                        listening_for_ww = False
-                else:
-                    if DEBUG:
-                        print("No sound detected")
+    # Play notification sound
+    if NOTIFICATION_PATH:
+        sd.play(notification_wav, samplerate=AUDIO_OUTPUT_SAMPLE_RATE, device=AUDIO_OUTPUT_INDEX)
+        sd.wait()
 
     # Listen for query
-    with sd.RawInputStream(
-        dtype="int16",
-        channels=1,
-        callback=callback_record
-    ):
-        listening_for_query = True
+    text = wait_for_stt(sd, recognizer)
+    if text != "":
         if DEBUG:
-            print("Listening for query...")
-    
-        # Get text from query
-        while listening_for_query:
-            data = in_q.get()
-            if recognizer.AcceptWaveform(data):
+            print(f"Heard: {text}")
+    else:
+        if DEBUG:
+            print("No sound detected. Returning to wake word detection.")
+        continue
+    listening_for_query = False
 
-                # Perform speech-to-text (STT)
-                result = recognizer.Result()
-                result_dict = json.loads(result)
-                result_text = result_dict.get("text", "")
-                if result_text != "":
-                    if DEBUG:
-                        print(f"Heard: {result_text}")
-                else:
-                    if DEBUG:
-                        print("No sound detected. Returning to wake word detection.")
-                    continue
-                listening_for_query = False
-
-    # Perform predetermined actions for particular phrases
-    if result_text in ACTION_CLEAR_HISTORY:
+    # Perform actions for particular phrases
+    if text in ACTION_CLEAR_HISTORY:
         if DEBUG:
             print("ACTION: clearning history")
-        msg_history = []
+        msg_history = FixedSizeQueue(GPT_MAX_HISTORY)
         continue
-    if result_text in ACTION_STOP:
+    elif text in ACTION_STOP:
         if DEBUG:
             print("ACTION: stop listening")
         continue
-    else:
-        if DEBUG:
-            print("No actions detected. Querying chat backend.")
 
-    # Send text query to LLM
-    if DEBUG:
-        print(f"Sending: {result_text}")
-    
+    # Default action: query chat backend
+    else:
+
+        # Send request with limited reply length
+        if GPT_MAX_REPLY_SENTENCES > 0:
+            msg = text + f". Your response must be {GPT_MAX_REPLY_SENTENCES} sentences or fewer."
+        if DEBUG:
+            print(f"Sending: {msg}")
+        reply = query_chat(msg)
+        if DEBUG:
+            print(f"Received: {reply}")
+
+        # Repeat the query to get the full reply
+        # TODO: Looks like it's still giving us a truncated version. Come back and fix this.
+        if GPT_GET_FULL_REPLY:
+            msg = "Repeat the previous request, but do so without any limit on number of sentences."
+            if DEBUG:
+                print(f"Sending: {msg}")
+            reply_full = query_chat(msg)
+            if DEBUG:
+                print(f"Received: {reply}")
+
+        # Perform text-to-speech (TTS)
+        if TTS_ENABLE and reply:
+            if DEBUG:
+                print("Playing reply...")
+            wav = tts.tts(text=reply)
+            wav = np.array(wav) * AUDIO_OUTPUT_VOLUME
+            wav = resampy.resample(wav, TTS_MODEL_SAMPLE_RATE, AUDIO_OUTPUT_SAMPLE_RATE)
+            sd.play(wav, samplerate=AUDIO_OUTPUT_SAMPLE_RATE, device=AUDIO_OUTPUT_INDEX)
+            sd.wait()
--- test/tts/coqui-tts-test.py
+++ test/tts/coqui-tts-test.py
@@ -12,6 +12,10 @@ From: https://tts.readthedocs.io/en/latest/inference.html
 import subprocess
 import time
 
+import numpy as np
+import resampy
+import sounddevice as sd
+import soundfile as sf
 from TTS.api import TTS
 
 TEXT = "In a hole in the ground there lived a hobbit. Not a nasty, " \
@@ -20,13 +24,17 @@ TEXT = "In a hole in the ground there lived a hobbit. Not a nasty, " \
     "sit down on or to eat: it was a hobbit-hole, and that means " \
     "comfort."
 MODEL = "tts_models/en/ljspeech/speedy-speech"
+MODEL_SAMPLE_RATE = 22050   # Sample rate determined by model
 OUT_FILE = "out.wav"
+AUDIO_OUTPUT_INDEX = 1
+AUDIO_OUTPUT_SAMPLE_RATE = 48000    # Determined by speaker's capabilities
+VOLUME = 4.0
 
 # List models
 print("--- Models ---")
-models = TTS().list_models()
-for model in models:
-    print(model)
+print(TTS().list_models())
+# for model in models:
+#     print(model)
 print()
 
 # Initialize TTS
@@ -34,5 +42,18 @@ tts = TTS(model_name=MODEL, progress_bar=False)
 
 # Convert text to speech
 timestamp = time.time()
-tts.tts_to_file(text=TEXT, file_path=OUT_FILE)
+# tts.tts_to_file(text=TEXT, file_path=OUT_FILE)
+# wav, sample_rate = sf.read(OUT_FILE)
+# print(f"Sample rate: {sample_rate}")
+wav = tts.tts(text=TEXT)
+
+# Resample
+wav = np.array(wav) * VOLUME
+print(wav.shape)
+wav = resampy.resample(wav, MODEL_SAMPLE_RATE, AUDIO_OUTPUT_SAMPLE_RATE)
+
+# Play
+sd.play(wav, samplerate=AUDIO_OUTPUT_SAMPLE_RATE, device=AUDIO_OUTPUT_INDEX)
+sd.wait()
+
 print(f"TTS time: {time.time() - timestamp}")
